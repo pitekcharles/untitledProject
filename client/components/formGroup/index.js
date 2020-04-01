3c976b641dc5791364b6abe78a74ad70036224bd
Charles Pitek <charles.pitek@gmail.com>
2020-03-31 18:18:27 -0600

    created form group component

---
+++ client/components/formGroup/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function formGroup({ children }) {
+    return (
+        <div className="form-group">
+            {children}
+        </div>
+    );
+};
+
+export default formGroup;
