c7bbbbbf2a56aa38df0ccdee50887fce4cddae6d
Charles Pitek <charles.pitek@gmail.com>
2020-03-31 19:32:06 -0600

    created the container component

---
+++ client/components/container/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function container({ children }) {
+    return (
+        <div className="container">
+            {children}
+        </div>
+    );
+};
+
+export default container;
